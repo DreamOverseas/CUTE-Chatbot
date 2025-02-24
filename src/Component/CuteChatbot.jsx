6b76753abe8bf0b682477ede7297f98ecd643bd8
Hanny <zyhaierciyuan@gmail.com>
2025-02-24 14:05:32 +1100

    Branch creation
    small fixes

--- src/Component/CuteChatbot.jsx
+++ src/Component/CuteChatbot.jsx
@@ -36,8 +36,8 @@ const CuteChatbot = () => {
         >
           {/* In-window component */}
           <h1>Hello world</h1>
-          <p>, I love RUSTTTTTT! This is the sample chatpage should be.</p>
-          <input type="text" id="user message" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 absolute inset-x-0 bottom-2 left-2 right-2" placeholder="Write a messager..." required />
+          <p>, I love RUSTTTTTT! This is where the sample chatpage should be.</p>
+          <input type="text" id="user message" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 absolute inset-x-0 bottom-2 left-2 right-2" placeholder="Write a messager..." required />
         </div>
       )}
     </div>
