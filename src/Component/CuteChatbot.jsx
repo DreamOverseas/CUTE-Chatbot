9b67fef5a72481060e8e22d1fe38679c623f53a4
Hanny <zyhaierciyuan@gmail.com>
2025-02-27 13:23:36 +1100

    break down message sending method

--- src/Component/CuteChatbot.jsx
+++ src/Component/CuteChatbot.jsx
@@ -5,6 +5,7 @@
 // src/Components/CuteChatbot.js
 import { useState, useEffect } from 'react';
 
+import useSpeechToText from 'react-hook-speech-to-text';
 
 const CuteChatbot = () => {
   const [open, setOpen] = useState(false);
@@ -12,6 +13,7 @@ const CuteChatbot = () => {
   const [aiMessages, setAiMessages] = useState(["您好，请问我有什么可以帮您的？Good'ay. How can I help you today?"]);
   const [input, setInput] = useState("");
   const [isVisible, setIsVisible] = useState(false);
+  const [isHovered, setIsHovered] = useState(false); // For the send button
 
   const openaiApiUrl = import.meta.env.VITE_OPENAI_API_URL;
   const openaiAsstId = import.meta.env.VITE_OPENAI_ASST_ID;
@@ -46,7 +48,7 @@ const CuteChatbot = () => {
         // );
 
         // if (!assistantResponse.ok) {
-        //   console.err("Assistent API response error.");
+        //   console.error("Assistent API response error.");
         //   throw new Error("Failed to fetch assistant details");
         // }
 
@@ -83,14 +85,52 @@ const CuteChatbot = () => {
     };
 
     initializeChatbot();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  // 使用 react-hook-speech-to-text 进行语音识别
+  const {
+    error,
+    interimResult,  // 语音识别的临时结果
+    isRecording,    // 当前是否在录制
+    results,        // 已确认的识别结果
+    startSpeechToText,
+    stopSpeechToText,
+  } = useSpeechToText({
+    continuous: true,
+    useLegacyResults: false,
+  });
+
+  // 监听 interimResult 与 results，当语音识别返回内容时实时更新输入框
+  useEffect(() => {
+    // 如果有临时结果，则更新输入框内容
+    if (interimResult) {
+      setInput(interimResult);
+    }
+    // 如果没有临时结果，但有最终识别结果（例如录制结束后），则取最新一条结果更新
+    else if (results.length > 0) {
+      const lastTranscript = results[results.length - 1].transcript;
+      setInput(lastTranscript);
+    }
+  }, [interimResult, results]);
+
+  // 点击录制按钮时的处理函数
+  const handleRecordClick = () => {
+    if (!isRecording) {
+      startSpeechToText();
+    } else {
+      stopSpeechToText();
+    }
+  };
+
+  if (error) { console.error(error) };
 
   // const testButtonFunc = () => {
   //   console.log("Assistant found:");
   //   console.log(assistant);
   //   console.log(`and thread ${threadId} created.`);
   // }
+
   // Send message to Assistant
   const sendMessageToAssistant = async (userMessage) => {
     if (!threadId) {
@@ -207,7 +247,7 @@ const CuteChatbot = () => {
           if (latestAiMessage) {
             aiResponse = latestAiMessage.content[0].text.value;
             setAiMessages((prev) => [...prev, aiResponse]);
-            
+
             console.log("AI Response:", aiResponse);//TODO: Delete for test purpose
           }
         }
@@ -218,10 +258,14 @@ const CuteChatbot = () => {
     finally { setAiThinking(false); } // Stop Thinking
   };
 
+  const sendNow = () => {
+    sendMessageToAssistant(input.trim());
+    setInput("");
+  }
+
   const handleKeyDown = (e) => {
     if (e.key === "Enter" && input.trim()) {
-      sendMessageToAssistant(input.trim());
-      setInput("");
+      sendNow();
     }
   };
 
@@ -258,11 +302,10 @@ const CuteChatbot = () => {
       {/* Inner components here */}
       {open && (
         <div
-        id="chat card"
-        className={`card position-fixed bottom-24 right-5 w-96 max-w-[90%] min-h-3/4 max-h-3/4 shadow-md lg:w-1/3 transition-opacity duration-300 ease-in-out ${
-          isVisible ? 'opacity-100' : 'opacity-0'
-        }`}
-      >
+          id="chat card"
+          className={`card position-fixed bottom-24 right-5 w-96 max-w-[90%] min-h-3/4 max-h-3/4 shadow-md lg:w-1/3 transition-opacity duration-300 ease-in-out ${isVisible ? 'opacity-100' : 'opacity-0'
+            }`}
+        >
           {/* In-window component */}
 
           {loading ?
@@ -306,42 +349,54 @@ const CuteChatbot = () => {
 
           {/* Input Field */}
           {aiThinking ? (
-            <div className="relative w-full">
-              <input
-                type="text"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full opacity-50 cursor-not-allowed"
-                placeholder="Thinking Hard..."
-                value={input}
-                disabled
-              />
-              <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5 text-gray-400 animate-spin fill-gray-600"
-                  viewBox="0 0 100 101"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                    fill="currentColor"
-                  />
-                  <path
-                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                    fill="currentFill"
-                  />
-                </svg>
+            <div className="flex items-center">
+              <div className='relative w-full'>
+                <input
+                  type="text"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 w-full opacity-50 cursor-not-allowed"
+                  placeholder="Thinking Hard..."
+                  value={input}
+                  disabled
+                />
+                <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+                  <svg
+                    aria-hidden="true"
+                    className="w-5 h-5 text-gray-400 animate-spin fill-gray-600"
+                    viewBox="0 0 100 101"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                      fill="currentColor"
+                    />
+                    <path
+                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                      fill="currentFill"
+                    />
+                  </svg>
+                </div>
               </div>
             </div>
           ) : (
-            <input
-              type="text"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-full"
-              placeholder="Write a message..."
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              onKeyDown={handleKeyDown}
-            />
+            <div className="flex items-center">
+              {/* Start STT */}
+              <button
+                onClick={handleRecordClick}
+                className={`mr-2 focus:outline-none ${isRecording ? 'text-red-500' : 'text-gray-500'}`}
+              >
+                <i className={isRecording ? "bi bi-record-circle-fill" : "bi bi-record-circle"}></i>
+              </button>
+              {/* Input box */}
+              <input
+                type="text"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-full"
+                placeholder="Write a message..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />
+            </div>
           )}
         </div>
       )}
