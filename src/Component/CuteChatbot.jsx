8f6109b1c1687037265376b6f38431604714dd1a
Hanny <zyhaierciyuan@gmail.com>
2025-02-27 13:25:06 +1100

    Sending buttons with 3 states

--- src/Component/CuteChatbot.jsx
+++ src/Component/CuteChatbot.jsx
@@ -377,6 +377,12 @@ const CuteChatbot = () => {
                   </svg>
                 </div>
               </div>
+              <button
+                disabled
+                className="p-2 text-green-500 cursor-not-allowed border-0 outline-none focus:outline-none"
+              >
+                <i className="bi bi-send-check-fill text-xl"></i>
+              </button>
             </div>
           ) : (
             <div className="flex items-center">
@@ -396,6 +402,28 @@ const CuteChatbot = () => {
                 onChange={(e) => setInput(e.target.value)}
                 onKeyDown={handleKeyDown}
               />
+              {/* Send Button */}
+              <button
+                onClick={sendNow}
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+                className="p-2 text-gray-600 hover:text-blue-600 transition-colors border-0 outline-none focus:outline-none"
+              >
+                {isHovered ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-send-fill text-xl"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471z" />
+                  </svg>
+                ) : (
+                  <i className="bi bi-send text-xl"></i>
+                )}
+              </button>
             </div>
           )}
         </div>
