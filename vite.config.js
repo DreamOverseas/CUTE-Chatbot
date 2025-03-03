c6cb6228ffa5f2c439d6e0701f24e1f62c05fe2d
Hanny <zyhaierciyuan@gmail.com>
2025-03-03 15:21:08 +1100

    More configs

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: './src/Component/CuteChatbot.jsx', // Entry
-      name: 'CUTE Chatbot',
+      name: 'CuteChatbot',
       fileName: 'CuteChatbot',
       formats: ['es', 'cjs'],
     },
